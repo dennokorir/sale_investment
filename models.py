87b0f137c716d3488316fae376da9a5c68c44e1c
Denis Korir <dennokorir@gmail.com>
2016-02-14 20:25:36 +0300

    First Commit

---
+++ models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class sale_investments(models.Model):
+#     _name = 'sale_investments.sale_investments'
+
+#     name = fields.Char()
